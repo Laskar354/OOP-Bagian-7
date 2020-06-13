038e523a6ea1731fc7256d24b6d8ad52643b29fb
Laskar354 <61928924+Laskar354@users.noreply.github.com>
2020-06-13 17:53:11 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//gunakan Abstraction.AbstractClass; untuk AbstractClass
+//gunakan AbstractClass.Interface; untul Interface
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Abstract Class
+            
+            /*Hewan hewan;
+            Console.WriteLine();
+            hewan = new Penjelasan();
+            hewan.PenggolonganMenurutMakanan();
+            Console.WriteLine();
+            Console.WriteLine("Penggolongan Hewan Menurut Jenis Makanannya : ");
+            Console.WriteLine();
+            hewan = new Karnivora();
+            hewan.PenggolonganMenurutMakanan();
+            Console.WriteLine();
+            hewan = new Herbivora();
+            hewan.PenggolonganMenurutMakanan();
+            Console.WriteLine();
+            hewan = new Omnivora();
+            hewan.PenggolonganMenurutMakanan();*/
+
+            
+            //Interface
+
+            /*IHewan hewan;
+            Console.WriteLine();
+            hewan = new Penjelasan();
+            hewan.PenggolonganMenurutMakanan();
+            Console.WriteLine();
+            Console.WriteLine("Penggolongan Hewan Menurut Jenis Makanannya : ");
+            Console.WriteLine();
+            hewan = new Karnivora();
+            hewan.PenggolonganMenurutMakanan();
+            Console.WriteLine();
+            hewan = new Herbivora();
+            hewan.PenggolonganMenurutMakanan();
+            Console.WriteLine();
+            hewan = new Omnivora();
+            hewan.PenggolonganMenurutMakanan();*/
+
+            Console.ReadKey();
+
+        }
+    }
+}
